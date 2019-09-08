3d5f8e6d1989a706927c5720104f12f200932ddd
alexkupriyanov <33920554+alexkupriyanov@users.noreply.github.com>
2019-09-08 22:54:01 +0700

    Update class.cs

--- class.cs
+++ class.cs
@@ -3,8 +3,12 @@ using System;
 
 public sealed class Singleton
 {
+    public static string text = "Test";
+    public string Date { get; private set; }
+
     private Singleton()
     {
+        Date = DateTime.Now.TimeOfDay.ToString();
     }
 
     public static Singleton Instance { get { return Nested.instance; } }
@@ -18,3 +22,5 @@ public sealed class Singleton
         internal static readonly Singleton instance = new Singleton();
     }
 }
+//Данная реализация является потокобезопасной, а также данные будут созданы либо в момент создания 1 экземпляра класса, 
+//либо при 1 обращении к статическим членам класса
